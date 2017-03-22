cea92e84acd0fbcd92e7edf32dc8af860dc2f1dc
hosi <nanakamado1632@gmail.com>
2017-03-22 15:13:14 +0000

    322

---
+++ test-carousel.js
@@ -0,0 +1,12 @@
+// JavaScript File
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
